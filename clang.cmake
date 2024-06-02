750c418da5f93c5fd9f42419e6e58d7d0151f62e
Alexander Beliaev <beliaevs@gmail.com>
2024-06-03 01:12:25 +0400

    toolchain files and gitignore

---
+++ clang.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER /usr/bin/clang)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++)
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
